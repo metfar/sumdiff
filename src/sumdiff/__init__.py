66fa416c75aa7cd27896bb07ea9d630a9446aa37
metfar <metfar@gmail.com>
2026-09-03 20:07:11 -0300

    Update sumdiff-0.2.8

--- src/sumdiff/__init__.py
+++ src/sumdiff/__init__.py
@@ -20,7 +20,7 @@
 #  MA 02110-1301, USA.
 #  
 #
-__version__ = "0.2.7";
+__version__ = "0.2.8";
 
 from .core import ComparisonSession, DiffHunk, DiffResult, DocumentState, compare_texts, intraline_spans;
 
